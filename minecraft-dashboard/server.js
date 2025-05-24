ef73d7c5c0d58a7e6d460aea44edbbe68ec85029
Nouman <noumanpm@gmail.com>
2025-05-25 00:55:56 +0530

    Webapp v2

--- minecraft-dashboard/server.js
+++ minecraft-dashboard/server.js
@@ -5,6 +5,8 @@ const app = express();
 // Set EJS as the view engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
 // Serve static files
 app.use(express.static(path.join(__dirname, "public")));
@@ -12,6 +14,33 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+const leaderboard = [
+  { username: 'Steve', questsCompleted: 5 },
+  { username: 'Alex', questsCompleted: 3 },
+  { username: 'EnderGuy', questsCompleted: 2 },
+];
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // For demo: Accept all credentials
+  console.log(`Login request from ${username}`);
+  res.redirect('/wallet');
+});
+
+app.get('/wallet', (req, res) => {
+  res.render('wallet');
+});
+
+app.get('/leaderboard', (req, res) => {
+  res.render('leaderboard', { leaderboard });
+});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
